8bb68b5ce3d60343d5a8ce22c94ae2a8a8ae7d8e
mprasad712 <manasprasad712@gmail.com>
2026-05-05 19:40:37 +0530

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,46 @@
+export type ErrorAlertType = {
+  title: string;
+  list: Array<string> | undefined;
+  id: string;
+  removeAlert: (id: string) => void;
+};
+export type NoticeAlertType = {
+  title: string;
+  link?: string;
+  id: string;
+  removeAlert: (id: string) => void;
+  list?: Array<string>;
+};
+export type SuccessAlertType = {
+  title: string;
+  id: string;
+  removeAlert: (id: string) => void;
+};
+export type SingleAlertComponentType = {
+  dropItem: AlertItemType;
+  removeAlert: (index: string) => void;
+  navigateTo?: string;
+  onClosePanel?: () => void;
+  isSeen?: boolean;
+};
+export type AlertDropdownType = {
+  children: JSX.Element;
+  notificationRef?: React.RefObject<HTMLDivElement>;
+  onClose?: () => void;
+  serverNotifications?: Array<{
+    id: string;
+    title: string;
+    link?: string | null;
+    created_at?: string;
+  }>;
+  markServerNotificationRead?: (id: string) => void;
+  markAllServerNotificationsRead?: () => void;
+};
+export type AlertItemType = {
+  type: "notice" | "error" | "success";
+  title: string;
+  link?: string;
+  list?: Array<string>;
+  id: string;
+  created_at?: string;
+};
